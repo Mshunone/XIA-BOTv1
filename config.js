88c901cc3139fcde349231d05c3e1167a3f8703c
Kasanstore7 <114809701+Kasanstore7@users.noreply.github.com>
2022-12-18 10:22:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-global.you = ['6285850539404'] 
+global.you = ['6285641142178'] 
 //Developer Bot
-global.gc = 'https://chat.whatsapp.com/I5Kz3Abawc5DqyBSDoYvKB'
-global.rowner = ['6285850539404'] // Real Owner Bot
-global.owner = ['6285850539404'] // Owner Bot
-global.police = ['6285850539404'] // Police Bot
-global.mods = ['6285850539404']  // Want some help?
-global.prems = ['6285850539404'] // Premium user has unlimited limit
+global.gc = 'https://chat.whatsapp.com/GeWPLmclHaVHsF0GymCcJz'
+global.rowner = ['6285641142178'] // Real Owner Bot
+global.owner = ['6283805685278'] // Owner Bot
+global.police = ['6285641142178'] // Police Bot
+global.mods = ['6285641142178']  // Want some help?
+global.prems = ['6285641142178'] // Premium user has unlimited limit
 global.fotonya1 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini kalo mau di ganti silahkan ganti aja
 global.fotonya2 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //gw lupa nih foto apa aja:v
 global.fotonya3 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini juga ganti
@@ -13,7 +13,7 @@ global.fotonya4 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini juga
 global.fotonya5 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini juga ganti
 global.fotonya6 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini ganti pakai foto sosialmedia mu
 global.fotonya7 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg'
-global.instagram = 'https://instagram.com/Jarotr_'
+global.instagram = 'https://github.com/Kasanstore7'
 
 global.helper = ['6285641142178']
 global.APIs = { // API Prefix
