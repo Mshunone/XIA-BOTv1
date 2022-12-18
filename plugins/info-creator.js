63ad7ac5cf5de2ad16a026704c6b0dc4773c73be
Kasanstore7 <114809701+Kasanstore7@users.noreply.github.com>
2022-12-18 10:31:49 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -26,24 +26,24 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:;;;
-FN: ᯤ 𝙅𝙖𝙧𝙤𝙩 𝙊𝙛𝙘
-item.ORG: ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ
-item1.TEL;waid=6285850539404:6285850539404@s.whatsapp.net
-item1.X-ABLabel: ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ
-item2.EMAIL;type=INTERNET: Jarott@gmail.com
+FN: ᯤ Kasan OFcc
+item.ORG: Kasan Yt
+item1.TEL;waid=6285641142178:6285641142178@s.whatsapp.net
+item1.X-ABLabel: Vcard
+item2.EMAIL;type=INTERNET: sannstore272@gmail.com
 item2.X-ABLabel:📫 Gmail
 item3.ADR:;;📍 Acehnese - Indonesian;;;;
 item3.X-ABADR:ac
 item3.X-ABLabel:📮 Location Owner
-item4.URL:https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
+item4.URL: Wa.me/6285641142178
 item4.X-ABLabel:Youtube
-item5.URL:https://github.com/Alpiii22
+item5.URL:https://github.com/Kasanstore7
 item5.X-ABLabel:Github
-item6.URL:https://instagram.com/jarotr_
-item6.X-ABLabel:Instagram Jarot
+item6.URL:https://github.com/Kasanstore7
+item6.X-ABLabe://github
 item7.URL:http://Instagram.com/alpiii.rsy
 item7.X-ABLabel: Instagram Alpi
-item1.TEL;waid=6285943036511:6285850539404@s.whatsapp.net
+item1.TEL;waid=6285641142178:6285641142178@s.whatsapp.net
 item1.X-ABLabel: Creator Bot
 END:VCARD`
 const sentMsg  = await conn.sendMessage(
