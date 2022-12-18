ed4323ed5ce6a55cf75709e0264e64fc7a9f58b4
Kasanstore7 <114809701+Kasanstore7@users.noreply.github.com>
2022-12-18 10:28:05 +0700

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 「 Suport Bot Dengan Cara donasi 」
 
-⫹⫺ Via Pulsa Indosat : 6285850539404
-⫹⫺ Via Dana : 6285850539404
+⫹⫺ DONASI LAH BROH
+⫹⫺ Via Dana : 6283192405363
 ⫹⫺ Via Saweria :
-⫹⫺ https://saweria.co/jarotoffc
+⫹⫺ https://saweria.co/KasanStore
 
 `.trim()
   m.reply(text)
